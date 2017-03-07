8cdf30f933ea4e9383c5d56850e6bbc1b7a26794
xiaoyu-lyt <xiaoyu_lyt@163.com>
2017-03-07 11:15:12 +0800

    init project

---
+++ YLZDevelopmentKit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YLZDevelopmentKit
+//
+//  Created by 萧宇 on 07/03/2017.
+//  Copyright © 2017 萧宇. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YLZDevelopmentKit/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  YLZDevelopmentKit
+//
+//  Created by 萧宇 on 07/03/2017.
+//  Copyright © 2017 萧宇. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
